7ed97b04840974d3c015f4b87066c52a01bf17ea
Rskth <assisantonio495@gmail.com>
2026-05-02 22:14:03 -0300

    eficiencia

--- src/eficiencia.js
+++ src/eficiencia.js
@@ -1,13 +1,13 @@
 //eficiencia
 function calcularEficiencia() {
-    const totalDistancia  = rotas.reduce((acc, rota) => acc + rota.distancia, 0)
+    const totalDistancia = rotas.reduce((acc, rota) => acc + rota.distancia, 0)
     const totalTempo = rotas.reduce((acc, rota) => acc + parseFloat(rota.tempoEstimado), 0);
 
     const kmMedio = (totalDistancia / rotas.length).toFixed(2);
     const tempoMedio = (totalTempo / rotas.lenght).toFixed(2)
 
-
-
-
-
+    return {
+        kmMedio,
+        tempoMedio
+    };
 }
